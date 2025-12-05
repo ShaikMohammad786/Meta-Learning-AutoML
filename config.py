ab96d8f86aae9f490b9727d9e48407b215375b75
Shaik Mohammad <skmohammad378@gmail.com>
2025-12-05 18:37:39 +0530

    deleted

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-JWT_SECRET = os.getenv("JWT_SECRET", "supersecret")
-JWT_ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
--- pydantic_models/task_type.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-class TaskType(str, Enum):
-    classification = "classification"
-    regression = "regression"
